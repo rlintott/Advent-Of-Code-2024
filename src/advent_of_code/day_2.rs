78cc692605f2ace557199c92422603b428962009
Ryan Lintott <ryan.m.lintott@gmail.com>
2024-12-08 13:12:44 -0800

    complicated trait stuff...

--- src/advent_of_code/day_2.rs
+++ src/advent_of_code/day_2.rs
@@ -17,6 +17,7 @@ fn decreasing(a: i32, b: i32) -> bool {
 
 fn jumps_with_range(a: i32, b: i32) -> bool {
     let jump = (b - a).abs();
+    //dbg!(jump);
     jump >= 1 && jump <= 3
 }
 
@@ -31,9 +32,9 @@ impl advent_of_code::Day for Day2 {
                 .collect()
             })
             .filter(|x: &Vec<i32>| { 
-                (check_safety(x, false, increasing) || 
-                check_safety(x, false, decreasing)) &&
-                check_safety(x, false, jumps_with_range)
+                (check_safety(x.iter(), false, increasing).0 || 
+                check_safety(x.iter(), false, decreasing).0) &&
+                check_safety(x.iter(), false, jumps_with_range).0
             })
             .count();
 
@@ -48,9 +49,23 @@ impl advent_of_code::Day for Day2 {
                 .collect()
             })
             .filter(|x: &Vec<i32>| { 
-                (check_safety(x, true, increasing) || 
-                check_safety(x, true, decreasing)) &&
-                check_safety(x, true, jumps_with_range)
+                let mut safety: (bool, Option<usize>) = check_safety(x.iter(), true, increasing);
+                if safety.0 == false {
+                    safety = check_safety(x.iter(), true, decreasing);
+                }      
+                if safety.0 == false {
+                    return false;
+                }
+                if let Some(skip_index) = safety.1 {
+                    dbg!(x);
+                    dbg!(skip_index);
+                    let result = check_safety(x.iter().skip(skip_index), false, jumps_with_range).0;
+                    dbg!(result);
+                    result
+                }
+                else {
+                    check_safety(x.iter(), true, jumps_with_range).0
+                }
             })
             .count();
 
@@ -58,47 +73,49 @@ impl advent_of_code::Day for Day2 {
     }
 }
 
-fn check_safety<F>(input: &[i32], allow_removal: bool, condition: F) -> bool where 
+
+fn check_safety<'a, F>(input: impl Iterator<Item = &'a i32>, allow_removal: bool, condition: F) -> (bool, Option<usize>) where 
 F: Fn(i32, i32) -> bool {
-    let mut iter = input.iter().peekable();
+    //dbg!(input);
+    let mut iter = input.enumerate().peekable();
     let mut prev = iter.next();
     let mut already_removed = false;
-    dbg!(input);
-
+    let mut is_first_elem: bool = true;
+    let mut index_removed: Option<usize> = None; 
     while let Some(mut curr) = iter.next() {
+        //dbg!(prev.unwrap());
+        //dbg!(curr);
+        let prev_val: &i32 = prev.unwrap().1;
+        let curr_val: &i32 = curr.1;
 
-        if allow_removal && already_removed == false && iter.peek().is_none() {
-            // edge case: last element
-            return true
-        }
-        
-        if prev.is_some() {
-            if condition(*prev.unwrap(), *curr) == false {
-                // about to fail
-                if allow_removal 
-                        && already_removed == false 
-                        && iter.peek().is_some() 
-                        && condition(*prev.unwrap(), **(iter.peek().unwrap())) {
-                    // if we havent already removed a level we remove this one
-                    already_removed = true;
-                    curr = iter.next().unwrap();
-                } else {
-                    dbg!(false);
-                    return false;
-                }
+        if condition(*prev_val, *curr_val) == false {
+
+            // about to fail
+            if allow_removal 
+                    && already_removed == false 
+                    && iter.peek().is_some() 
+                    && condition(*prev_val, *(iter.peek().unwrap()).1) {
+                // if we havent already removed a level we remove this one
+                already_removed = true;
+                curr = iter.next().unwrap();
+                index_removed = Some(curr.0);
+            } else if allow_removal && is_first_elem {
+                already_removed = true;
+                curr = iter.next().unwrap();
+                index_removed = Some(curr.0);
+            } else if allow_removal && iter.peek().is_none() {
+                index_removed = Some(curr.0);
+                return (true, index_removed);
+            } else {
+                //dbg!(false);
+                //dbg!(input);
+                return (false, index_removed);
             }
-        } else if allow_removal 
-                && prev.is_none() 
-                && condition(*curr, **(iter.peek().unwrap())) == false {
-            // edge case: if this is the first element and it is invalid, we need to skip it
-            already_removed = true;
-            curr = iter.next().unwrap();
         }
         prev = Some(curr);
+        is_first_elem = false;
     }
-    // 100, 3, 101, 5, 7
+    // 100, 3, 101, 102, 103
     // 100, 3, 4, 5, 7
-
-    dbg!(true);
-    true
+    (true, index_removed)
 }
